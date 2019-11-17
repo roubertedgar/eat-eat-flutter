5e0576f0f60160abcb189b01a1a91cd95e21faf6
Roubert Edgar <roubertedgar@gmail.com>
2019-11-17 14:53:12 -0300

    `Updates flutter versions

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/roubertedgar/Library/Flutter/sdk"
+export "FLUTTER_APPLICATION_PATH=/Users/roubertedgar/Projects/personal/eat-eat-flutter"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/roubertedgar/Library/Flutter/sdk/bin/cache/artifacts/engine/ios"
